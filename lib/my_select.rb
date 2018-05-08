19d3369abaec2c1b853dacd8d250f01a8858e481
jee <jag918@nyu.edu>
2018-05-08 03:54:42 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,6 +1,11 @@
 def my_select(collection)
  i = 0
+ new_collection = []
  while i < collection.length
+  should_add = yield collection[i] 
+  if should_add
+    new_collection << collection[i]
+  end
   i = i +1
  end
 end
